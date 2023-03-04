e9a10a8befefd944b02fc313ca1c6971477fa256
tnguyen606-cs <tnguyen606@gatech.edu>
2023-03-03 20:55:24 -0500

    Created user account info

--- app/Forms/form.py
+++ app/Forms/form.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
@@ -6,7 +8,7 @@ from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
-                           DataRequired(), Length(min=8, max=21)])
+                           DataRequired(), Length(min=5, max=21)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
@@ -28,8 +30,35 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[
-                           DataRequired(), Length(min=8, max=21)])
+                           DataRequired(), Length(min=5, max=21)])
     password = PasswordField('Password', validators=[DataRequired()])
     # This helps Users still stay login for a little while after closing the app
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[Length(min=5, max=21)])
+    email = StringField('Email',
+                        validators=[Email()])
+    new_password = PasswordField('New Password')
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     EqualTo('new_password')])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'That email is taken. Please choose a different one.')

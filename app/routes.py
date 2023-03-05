cb4972eaeaf891feae701ded2e1104019dd36e30
tnguyen606-cs <tnguyen606@gatech.edu>
2023-03-05 13:02:48 -0500

    fixed empty password

--- app/routes.py
+++ app/routes.py
@@ -114,6 +114,7 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
+        form.new_password.data = current_user.image_file
     image_file = url_for(
         'static', filename='images/' + current_user.image_file)
     return render_template('account.html', title='Account',
